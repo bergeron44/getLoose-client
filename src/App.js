27d3781cc08f5fc8b5cc992008fe9f3ddbabc607
ron berger <ronberger40@gmail.com>
2024-09-06 14:25:30 +0300

    little changes to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePageForDates from './pages/HomePageForDates';
 import HomePageForFriends from './pages/HomePageForFrindes'; 
 import FirstPageDateOrFrinde from './pages/FirstPageDateOrFrinde';
 import ChooseDate from './pages/ChooseDate';
-import ChooseFrindes from './pages/ChooseFriends' ;
+import ChooseFrindes from './pages/ChooseFriends';
 import AnimatedLogo from './components/AnimatedLogo';  // Import the AnimatedLogo component
 import BackToGame from './components/BackToGame'; 
 import WaitingForApproval from './pages/WaitingForApproval';
@@ -12,34 +12,31 @@ import DateGame from './pages/DateGame';
 import FrindesGame from './pages/FrindesGame';
 import Instruction from './pages/Instruction';
 import './App.css';
-//bar pages implement
+
+// Bar pages
 import BarTable from './barLoginPages/BarTable';
 import NotInBar from './barLoginPages/NotInBar';
 import TheRustyAnchor from './barLoginPages/The_Rusty_Anchor';
 import Admin from './barLoginPages/Admin';
-//end bar pages 
-//add to data base pages
-import  AddQuestion  from './pages/DataBaseUiConection/AddQuestion';
-import  AddBar  from './pages/DataBaseUiConection/AddBar';
-import  AddPackage  from './pages/DataBaseUiConection/AddPackage';
-//end add to data base pages
+
+// Add to database pages
+import AddQuestion from './pages/DataBaseUiConection/AddQuestion';
+import AddBar from './pages/DataBaseUiConection/AddBar';
+import AddPackage from './pages/DataBaseUiConection/AddPackage';
+
 import DynamicPage from './barLoginPages/DynamicPage'; 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        
         <div className="page-content">
           <Routes>
             <Route path="/" element={<FirstPageDateOrFrinde />} />
-            <Route path="/:path" element={<DynamicPage />} /> 
-            <Route path="/FirstPageDateOrFrinde" element={<FirstPageDateOrFrinde />} />
+            <Route path="/Admin" element={<Admin />} />
             <Route path="/BarTable" element={<BarTable />} />
             <Route path="/NotInBar" element={<NotInBar />} />
             <Route path="/TheRustyAnchor" element={<TheRustyAnchor />} />
-            <Route path="/Admin" element={<Admin />} />
-
             <Route path="/LogoPage" element={<AnimatedLogo />} />
             <Route path="/BackToGame" element={<BackToGame />} />
             <Route path="/HomePageForDates" element={<HomePageForDates />} />
@@ -47,13 +44,15 @@ function App() {
             <Route path="/ChooseDate" element={<ChooseDate />} />
             <Route path="/ChooseFrindes" element={<ChooseFrindes />} />
             <Route path="/DateGame" element={<DateGame />} />
-            <Route path="/FrindesGame" element={<FrindesGame/>} />
-            <Route path="/page3" element={<FirstPageDateOrFrinde />} />
+            <Route path="/FrindesGame" element={<FrindesGame />} />
             <Route path="/WaitingForApproval" element={<WaitingForApproval />} />
             <Route path="/AddQuestion" element={<AddQuestion />} />
             <Route path="/AddBar" element={<AddBar />} />
             <Route path="/AddPackage" element={<AddPackage />} />
             <Route path="/Instruction" element={<Instruction />} />
+            
+            {/* Catch-all route for dynamic paths */}
+            <Route path="*" element={<DynamicPage />} />
           </Routes>
         </div>
         <h1 className="graffiti-headline">Get Loose</h1>
