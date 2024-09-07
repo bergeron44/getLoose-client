508762eeef3f83540efe5c89be46b98c0dd21c3e
ron berger <ronberger40@gmail.com>
2024-09-07 16:33:36 +0300

    hey

--- src/barLoginPages/NavigatePage.js
+++ src/barLoginPages/NavigatePage.js
@@ -9,6 +9,7 @@ const NavigatePage = () => {
     // Extract the barName from the URL parameters
     const queryParams = new URLSearchParams(location.search);
     const barName = queryParams.get('barName');
+    console.log(barName);
 
     // Check if barName exists, then navigate
     if (barName) {
@@ -18,9 +19,10 @@ const NavigatePage = () => {
 
   return (
     <div>
-      <h1>Loading...</h1>
+      <h1>bro this game you have to play in aBar</h1>
     </div>
   );
 };
 
 export default NavigatePage;
+//https://getloose.onrender.com/?barName=Admin
